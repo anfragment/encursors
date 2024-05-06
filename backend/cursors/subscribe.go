c9f6fcdc2efdf59429665a1246636c117a4c8991
Ansar Smagulov <ansar@anfragment.net>
2024-05-06 16:41:10 +0500

    Disable websocket.Upgrader origin check

--- backend/cursors/subscribe.go
+++ backend/cursors/subscribe.go
@@ -11,7 +11,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 type CursorEnterWebsocketPayload struct {
 	Id      string   `json:"id"`
